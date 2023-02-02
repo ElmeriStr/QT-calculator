5cbebff0d958e4b919d6485ea9b8d264256b8879
elmeristrom <stromelmeri@gmail.com>
2023-02-02 20:09:49 +0200

    initial commit from schools repository

---
+++ calculator/calculator.h
@@ -0,0 +1,33 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Calculator;
+}
+
+class Calculator : public QMainWindow
+{
+
+    Q_OBJECT
+
+public:
+
+
+    explicit Calculator(QWidget *parent = 0);
+    ~Calculator();
+
+private:
+    Ui::Calculator *ui;
+
+
+private slots :
+    void NumPressed();
+    void MathButtonPressed();
+    void EqualButtonPressed();
+    void ChangeNumberSign();
+    void on_Clear_clicked();
+};
+
+#endif // CALCULATOR_H
